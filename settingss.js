2f8bb16e59432c79e4e2fd0dd199495ab0dc74f5
weendy005 <botmalvina4@gmail.com>
2025-08-09 18:02:32 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~QmlVwRQD#yFZcvsqcN3ukkH3URrv_LtpXpksOWQiZfZI2FvCPpfM",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2250715405077", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
